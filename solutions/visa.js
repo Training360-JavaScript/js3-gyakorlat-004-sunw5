8a64bf59e11f60433a6f656c4901eecd5a2e385f
Gabor Szasz <skywallalp@gmail.com>
2021-11-12 00:40:03 +0100

    validator function

--- solutions/visa.js
+++ solutions/visa.js
@@ -1,6 +1,6 @@
-const checkVisa = (visaNumber) => {
-  // const num = visaNumber.replaceAll(" ", "");
-  return /^4\d{15}$/.test(visaNumber);
-};
+// const checkVisa = (visaNumber) => {
+//   // const num = visaNumber.replaceAll(" ", "");
+//   return /^4\d{15}$/.test(visaNumber);
+// };
 
-export default checkVisa;
+// export default checkVisa;
